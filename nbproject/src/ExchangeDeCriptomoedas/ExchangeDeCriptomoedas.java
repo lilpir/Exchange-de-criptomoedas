7f61147c95c158f9ce3a133272f8273272d41f5d
Alexandre Domiciano Pierri <144851875+lilpir@users.noreply.github.com>
2024-05-06 09:39:35 -0300

    Add files via upload
    Jform do cadastro, login e das opções mas sem os controllers para verificação do usuario

---
+++ nbproject/src/ExchangeDeCriptomoedas/ExchangeDeCriptomoedas.java
@@ -0,0 +1,15 @@
+
+package ExchangeDeCriptomoedas;
+
+import View.Login;
+
+
+public class ExchangeDeCriptomoedas {
+
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
